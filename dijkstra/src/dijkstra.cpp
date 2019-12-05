f0d6cce5f31fc753eb5c3c3d18ff80907fc5e769
Maylee <mgagnon987@gmail.com>
2019-12-04 19:55:35 -0500

    should be all set

--- dijkstra/src/dijkstra.cpp
+++ dijkstra/src/dijkstra.cpp
@@ -9,15 +9,17 @@ using namespace std;
 
 #define sizeOfGraph 10
 
-/* Vertex left in Q/not visited yet with the minimum distance
- *
+/* Finding the vertex with the smallest distance from source out of the vertices in queue
+ * @param d The array which tracks the current distances
+ * @param visited The array which tracks whether the vertex has been visited already
+ * @return The vertex with the smallest distance
  */
 int minDist(int d[sizeOfGraph], bool visited[sizeOfGraph]){
 	int min = INT_MAX;
 	int minPos = 0;
 
 	for (int i = 0; i < sizeOfGraph; i++){
-		if (visited[i] == false && d[i] <= min){
+		if (visited[i] == false && d[i] <= min){ // check to see if unvisited if smaller than ones so far
 			min = d[i];
 			minPos = i;
 		}
@@ -48,16 +50,23 @@ int main() {
 	}
 	fin.close();
 
+	cout << "Vertices as given in matrix file. Ranging from 0 to the total number of vertices" << endl;
+	cout << "ie. row 0/col 0 is vertex 0 and in given example refers to vertex A" << endl;
 
-	//Input from user, start and end vertex
-	int source;
-	int dest;
+	//Input from user, start and end vertex // TODO some issues with error catching on input, if occurs should defaultsto 0
+	int source = 0;
+	int dest = 0;
 	cout << "enter starting vertex: " << endl;
 	cin >> source;
 	if (cin.bad()) {
 			cout << "Bad input. Exiting." << endl;
 			return 0;
 	}
+	/*if (isdigit(start) && start >= 0 && start < graphSize){
+		source = start;
+		cout << "Bad input. Must be vertex number. Exiting" << endl;
+		return 0;
+	}*/
 	cout << "Source: " << source << endl;
 
 	cout << "enter ending vertex: " << endl;
@@ -66,35 +75,40 @@ int main() {
 		cout << "Bad input. Exiting." << endl;
 		return 0;
 	}
+	/*if (isdigit(end) && end >= 0 && end < graphSize){
+		dest = end;
+		cout << "Bad input. Must be vertex number. Exiting" << endl;
+		return 0;
+	}*/
 	cout << "Dest: " << dest << endl;
 
 
 	//Dijkstra's algorithm
-	bool beenVisited[graphSize]; //vertex set Q
+	bool beenVisited[graphSize];
 	int vLeftCt = graphSize;
 	int dist[graphSize];
 	int prev[graphSize];
 
-	for (int i = 0; i < graphSize; i++){
+	for (int i = 0; i < graphSize; i++){ // initialize arrays
 		dist[i] = INT_MAX;
 		prev[i] = NULL;
-		beenVisited[i] = false; //add v to Q
+		beenVisited[i] = false;
 	}
 	dist[source] = 0;
 
-	while(vLeftCt > 0){ //  while Q not empty
-		int u = minDist(dist, beenVisited); //v w/ min dist & unvisited
+	while(vLeftCt > 0){
+		int u = minDist(dist, beenVisited);  // find vertex from queue with smallest dist, to use as the next starting vertex
 
-		beenVisited[u] = true;
+		beenVisited[u] = true; // remove from queue
 		vLeftCt--;
-		if (u == dest){
+		if (u == dest){ // stop because reach dest
 			break;
 		}
 
 		for (int i = 0; i < graphSize; i++){ // unvisited neighbors of u
 			if (beenVisited[i] == false && graph[u][i] != 0){
 				int tempDist = dist[u] + graph[u][i];
-				if (tempDist < dist[i]){
+				if (tempDist < dist[i]){ // new path/dist smaller, record
 					dist[i] = tempDist;
 					prev[i] = u;
 				}
